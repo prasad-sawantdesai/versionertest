e1d8731436b5aa3d23066a0a9f403267dc8f13d8
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2022-10-18 15:32:52 +0200

    updated

---
+++ _version.py
@@ -0,0 +1,5 @@
+git_refnames = "$Format:%d$"
+git_describe = "$Format:%(describe:tags=true)$"
+git_full = "$Format:%H$"
+git_date = "$Format:%ci$"
+keywords = {"refnames": git_refnames, "full": git_full, "date": git_date}
